70eb3f00d79753924387ac1c0dba550550dc3d93
Wmlsdd <164832580+Wmlsdd@users.noreply.github.com>
2026-05-15 16:51:45 +0800

    Add files via upload

--- set.d.mts
+++ set.d.mts
@@ -1,4 +1,4 @@
-import { PropertyPath } from '../_internal/PropertyPath.js';
+import { PropertyPath } from '../_internal/PropertyPath.mjs';
 
 /**
  * Sets the value at the specified path of the given object. If any part of the path does not exist, it will be created.
--- set.js
+++ set.js
@@ -1,1 +1,11 @@
-module.exports = require('./dist/set');
+'use strict';
+
+Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+
+const updateWith = require('./updateWith.js');
+
+function set(obj, path, value) {
+    return updateWith.updateWith(obj, path, () => value, () => undefined);
+}
+
+exports.set = set;
